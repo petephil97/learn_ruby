c3462279f9c96f724e5159bc8ec5aa3e1da431fa
Pete Phillips <petephil97@gmail.com>
2017-07-23 22:14:12 -0230

    Solved 03_simon_says by adding functions to perform various alterations to a string.

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
-#write your code here
+def echo say
+   say
+end
+
+def shout say
+   say.upcase
+end
+
+def repeat (say, num = 2)
+   ([say] * num).join(" ")
+end
+
+def start_of_word(say, num)
+   say[0...num]
+end
+
+def first_word say
+   say.split(" ").first
+end
+
+def titleize say
+   words = say.split.map do |word|
+      if %w(the and over).include?(word)
+         word
+      else
+         word.capitalize
+      end
+   end
+   words.first.capitalize!
+   words.join(" ")
+end
