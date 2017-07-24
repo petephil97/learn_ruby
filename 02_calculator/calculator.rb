4dbf0aa0fde5ac1848409f992fed4f46b5527865
Pete Phillips <petephil97@gmail.com>
2017-07-23 21:56:30 -0230

    Solved 02_calculator by adding various functions that perform math operations.

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,11 @@
-#write your code here
+def add(num1, num2)
+   num1 + num2
+end
+
+def subtract(num1, num2)
+   num1 - num2
+end
+
+def sum(numbers)
+   numbers.inject(0){|total, number| total+number}
+end
